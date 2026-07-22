f7bef733cead5daf1872226cf36a9d506600e45e
Jeremy Boynes <jeremy@boynes.com>
2026-07-22 06:56:16 +0100

    Provide an example generating an Area Notice message.

---
+++ examples/send_area_notice.rs
@@ -0,0 +1,50 @@
+use ais_rs::asms::area_notice::{Description, Position, Radius, Scale, SubArea};
+use ais_rs::asms::Linkage;
+use ais_rs::{asms, BroadcastApplicationMessage, Message, Minutes, MonthDayHourMinute, MMSI};
+use bytes::BytesMut;
+use nmea_codec::types::{AisChannel, Talker};
+use nmea_codec::{IntoVDM, NmeaCodec};
+use tokio_util::codec::Encoder;
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let notice = asms::area_notice::AreaNotice {
+        linkage: Linkage(501),
+        description: Description::MammalsInArea_ReduceSpeed,
+        start: MonthDayHourMinute { month: 7, day: 21, hour: 13, minute: 45 },
+        duration: Minutes(1440),
+        subareas: vec![
+            SubArea::Circle {
+                scale: Scale(0),
+                position: Position {
+                    latitude: (42.386320965494704 * 60_000.0) as i32,
+                    longitude: (-70.91003783738283 * 60_000.0) as i32,
+                    precision: 4,
+                },
+                radius: Radius(0),
+            },
+            SubArea::Circle {
+                scale: Scale(0),
+                position: Position {
+                    latitude: (42.386320965494704 * 60_000.0) as i32,
+                    longitude: (-70.91003783738283 * 60_000.0) as i32,
+                    precision: 4,
+                },
+                radius: Radius(4000),
+            },
+        ],
+    };
+
+
+    let ais_msg = Message::BinaryBroadcastMessage(BroadcastApplicationMessage {
+        repeat_count: Default::default(),
+        source: MMSI::new(3669999)?,
+        message: asms::BroadcastApplicationMessage::AreaNotice(notice),
+    });
+
+    let mut encoder = NmeaCodec::new();
+    let mut buf = BytesMut::with_capacity(1024);
+    let msg = ais_msg.into_vdm(Talker::AIS, None, Some(AisChannel::A)).messages().next().unwrap();
+    encoder.encode(msg, &mut buf)?;
+    assert_eq!(buf.as_ref(), b"!AIVDM,1,1,,A,803Owsh0EWm0gEe`2l0=v:>i=W9L000001giAn9dq;Wl0000,3*3E");
+    Ok(())
+}
